4337c404fa11893868a0bd3944f7e553afba6bf4
gongistar <gong.gistar@gmail.com>
2019-10-21 17:24:34 +0800

    加入部门CRUD,laravel-admin中show,edit的使用

--- app/Admin/Controllers/DepartmentController.php
+++ app/Admin/Controllers/DepartmentController.php
@@ -9,22 +9,36 @@
 namespace App\Admin\Controllers;
 
 use App\Department;
+use function foo\func;
 use Illuminate\Routing\Controller;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
+use Encore\Admin\Show;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Auth\Database\Administrator;
+use Encore\Admin\Controllers\HasResourceActions;
 
 class DepartmentController extends Controller
 {
+
+    use HasResourceActions;
+
+    protected $title = '部门';
+
+    protected function title()
+    {
+        return $this->title;
+    }
+
     protected function grid()
     {
-        $grid = new Grid(new Department);
+        $grid = new Grid(new Department());
 
         $grid->column('department_id', 'ID')->sortable();
         $grid->column('department_name', '组名');
         $grid->column('department_dec', '描述');
-
+        $grid->column('leader.name', '部门经理')->label();
+        $grid->column('members', '成员')->pluck('name')->label();
         return $grid;
     }
 
@@ -36,23 +50,51 @@ class DepartmentController extends Controller
         $form->text('department_dec', '部门描述')->rules('required|min:3');
 
         $form->select('leader_id', '部门领导')->options(Administrator::all()->pluck('username','id'));
-        
-        $form->multipleSelect('user_id', '组员')->options(Administrator::all()->pluck('username','id'));
+        $form->multipleSelect('members', '组员')->options(Administrator::all()->pluck('username','id'));
+
         return $form;
+    }
+
+    protected function detail($id)
+    {
+        $show = new Show(Department::findOrFail($id));
+
+        $show->field('department_id', 'ID');
+        $show->field('department_name', '部门名字');
+        $show->field('department_dec', '部门描述');
+
+        $show->field('leader','test')->as(function($leader){
+            return $leader->name;
+        })->label();
+        $show->field('members', '成员')->as(function ($member){
+            return $member->pluck('name');
+        })->label();
+
+        $show->field('created_at', trans('admin.created_at'));
+        $show->field('updated_at', trans('admin.updated_at'));
+
+        return $show;
     }
 
     protected function index(Content $content)
     {
-        return $content->title('部门')->description('列表')->body($this->grid());
+        return $content->title($this->title())->description('列表')->body($this->grid());
     }
 
     protected function create(Content $content)
     {
-        return $content->title('部门')->body($this->form());
+        return $content->title($this->title())->body($this->form());
     }
 
-    protected function store()
+    protected function show($id, Content $content)
     {
-        return $this->form()->store();
+        return $content->title($this->title())->body($this->detail($id));
+    }
+
+    protected function edit($id, Content $content)
+    {
+        return $content
+            ->title($this->title())
+            ->body($this->form()->edit($id));
     }
 }
--- app/Department.php
+++ app/Department.php
@@ -8,27 +8,27 @@
 
 namespace App;
 
+use Encore\Admin\Auth\Database\Administrator;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Encore\Admin\Auth\Database\Administrator;
 
 class Department extends Model
 {
 
     protected $table = 'admin_department';
 
-    protected $fillable = ['department_name', 'department_desc', 'leader_id', 'created_at', 'updated_at'];
+    protected $fillable = ['department_name', 'department_desc', 'leader_id', 'user_id', 'created_at', 'updated_at'];
 
     protected $primaryKey = 'department_id';
 
     public function members() : BelongsToMany
     {
-        return $this->belongsToMany('Administrator', 'admin_department_users', 'department_id', 'user_id');
+        return $this->belongsToMany('Encore\Admin\Auth\Database\Administrator', 'admin_department_users', 'department_id', 'user_id');
     }
 
     public function leader() : BelongsTo
     {
-        return $this->belongsTo('App\User', 'id', 'leader_id');
+        return $this->belongsTo('Encore\Admin\Auth\Database\Administrator',  'leader_id', 'id');
     }
 }
